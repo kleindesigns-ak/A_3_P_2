a5dc89664fea80cea3d0813a1828442f4d18804b
Austin Klein <austinwklein@gmail.com>
2022-11-09 10:45:34 -0900

    Initial commit : Zipcode <-> Postnet Convertor Stores in zipcode as int

---
+++ ZipCode.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Zipcode{
+public:
+    // Constructors
+    Zipcode();
+    explicit Zipcode(int zipcode);
+    explicit Zipcode(string postnetCode);
+    // Getters / Setters
+    int getZipcode();
+    string getPostnetCode();
+
+    void setZipcode(int zipcode);
+    void setPostnetCode(string postnet);
+private:
+    // Private Vars
+    int zipcode;
+    // Main Conversion Functions
+    int convertStringPostnetToIntegerZipcode(string &postnetCode);
+    string convertIntegerZipcodeToStringPostnet(int &zipcode);
+    // Private Int to String Helper Functions
+    static void removeFramingOnes(string &postnetCode);
+    static void dividePostnetStringIntoFive(string &postnetCode, string *fiveElementArray);
+    static void convertStringArrayToIntZip(int &zipcode, string *fiveElementArray);
+    // Private Int to String Helper Functions
+    static void intZipToStringZip(int &zipcode, string &postnetCode);
+    static void stringZipToPostnetCode(string &postnetCode);
+};
